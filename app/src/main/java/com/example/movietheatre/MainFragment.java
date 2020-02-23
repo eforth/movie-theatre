5d01f3134239823cb2143fa5f12ef7cb79c16621
Ervin Forth <ervinforthjnr@gmail.com>
2020-02-23 12:47:05 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/movietheatre/MainFragment.java
@@ -0,0 +1,72 @@
+package com.example.movietheatre;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
+public class MainFragment extends Fragment {
+
+    private BottomNavigationView bottomNavigationView;
+    private NavController navController;
+
+
+    public MainFragment() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        bottomNavigationView = view.findViewById(R.id.bottom_navigation);
+
+        navController = Navigation.findNavController(view.findViewById(R.id.nav_host_main));
+
+        setupBottomNavigation();
+    }
+
+    private void setupBottomNavigation() {
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        navController.navigate(R.id.homeFragment);
+                        return true;
+                    case R.id.action_feed:
+                        navController.navigate(R.id.homeFragment);
+                        //return true;
+                    case R.id.action_account:
+                        navController.navigate(R.id.homeFragment);
+                        //return true;
+                    case R.id.action_store:
+                        navController.navigate(R.id.homeFragment);
+                        //return true;
+                }
+                return false;
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/movietheatre/SplashFragment.java
@@ -0,0 +1,31 @@
+package com.example.movietheatre;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SplashFragment extends Fragment {
+
+
+    public SplashFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_splash, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        NavHostFragment.findNavController(this).navigate(R.id.mainFragment);
+    }
+}
